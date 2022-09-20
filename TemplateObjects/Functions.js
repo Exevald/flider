6d11b011c34fe44a86cd1a3dee6686f09ff8ce4b
DahaUbivaha <107624506+DahaUbivaha@users.noreply.github.com>
2022-09-20 14:41:52 +0300

    Прех + действ + слайд функции скелет
    0_0

---
+++ TemplateObjects/Functions.js
@@ -0,0 +1,63 @@
+function changeTitle() {
+    
+}
+
+function saveAsJSON() {
+
+}
+
+function saveAsPDF() {
+
+}
+
+function open() {
+    
+}
+
+function watch() {
+    
+}
+
+function changeCurrentSlide() {
+    
+}
+
+function showHistory() {
+    
+}
+
+function undo() {
+    
+}
+
+function redo() {
+    
+}
+
+function createSlide() {
+    
+}
+
+function deleteSlide() {
+    
+}
+
+function copy() {
+    
+}
+
+function move() {
+    
+}
+
+function changeBgColor() {
+    
+}
+
+function addItem() {
+    
+}
+
+function changeSelectedItem() {
+    
+}
