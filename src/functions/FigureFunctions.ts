54ea9709eac61497ce1b9fefdc0f5fae72eff233
Sergey <100339481+alozhka@users.noreply.github.com>
2022-09-29 00:19:49 +0300

    Добавил реализацию enum
    Возвращает название типа объекта через индекс элемента типа enum

--- src/functions/FigureFunctions.ts
+++ src/functions/FigureFunctions.ts
@@ -2,4 +2,7 @@ import {History, Actions, Slide, Item, Image} from "../model/Types";
 import {ItemType} from "../model/Types";
 
 function addFigure(type: number, coords: number, angle: number): void {
+    const newItem: Item {
+        type: ItemType = ItemType[type],
+    }
 }
