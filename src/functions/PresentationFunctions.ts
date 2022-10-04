db577ab8ea8005957b721aca76f5838997618775
Sergey <100339481+alozhka@users.noreply.github.com>
2022-10-04 15:11:37 +0300

    some new functions

--- src/functions/PresentationFunctions.ts
+++ src/functions/PresentationFunctions.ts
@@ -2,16 +2,29 @@
 // действия со слайдами
 
 import {Presentation, Slide } from "../model/Types";
-import * as fs from 'fs';
+import * as fs from "fs";
+
+
+function changeTitle(Pr: Presentation, title: string): Presentation {
+    Pr.title = title;
+    return Pr
+}
 
 function saveAsJSON(Pr: Presentation): string {
-    return JSON.stringify(Pr)
+    return JSON.stringify(Pr);
 }
 
 function saveAsPDF(Pr: Presentation) {
     // без понятия
 }
 
-function open(dest: string) {
-    
+function open(dest: string): Presentation {
+    // считал строку и сразу же распарсил в json
+    let jsonConfig: JSON = JSON.parse(fs.readFileSync(dest, "utf8"));
+    let Pr: Presentation = jsonConfig;
+    return Pr;
+}
+
+function watch(Pr: Presentation) {
+    // тут будет адская вёрстка
 }
