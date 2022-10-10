efb739dc5aad469ad99ff4e79d8b7271a7a25a98
Sergey <100339481+alozhka@users.noreply.github.com>
2022-10-11 00:20:25 +0300

    Начал писать всёрстку шапки

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,34 @@
 import React from 'react';
 import './App.css';
 
+function UtiltiesRow() {
+    return(
+        <div className="utilities-row">
+            <a href="">File</a>
+            <a href="">ToDo</a>
+            <a href="">Insert</a>
+            <a href="">Utilities</a>
+        </div>
+    )
+}
+
+function Header() {
+    return (
+        <div className="Header">
+            <UtiltiesRow />
+        </div>
+    )
+}
+
+
 function App() {
-  return (
-    <div className="App">
-      <h1>
-        Flider
-      </h1>
-    </div>
-  );
+    return (
+        <div className="App">
+            <h1>
+                Flider
+            </h1>
+        </div>
+    );
 }
 
 export default App;
