db9f48c69ec3d5d76022e4d5341ee5451b86525f
Sergey <100339481+alozhka@users.noreply.github.com>
2022-10-11 15:13:57 +0300

    Добавил действия с историей

--- src/functions/PresentationFunctions.ts
+++ src/functions/PresentationFunctions.ts
@@ -1,7 +1,11 @@
 // Включает сейвы, просмотр презентации;
 // действия со слайдами
-import {Presentation} from "../core/model/Types";
+import {History, Presentation} from "../core/model/Types";
 import * as fs from "fs";
+
+
+/**         очень важные константы          **/
+const MAX_HISTORY_SIZE = 30;
 
 
 function changeTitle(Pr: Presentation, title: string): Presentation {
@@ -16,25 +20,34 @@ function saveAsJSON(Pr: Presentation): string {
 function saveAsPDF(Pr: Presentation) {
     // без понятия
 }
-
-// function open(dest: string): Presentation {
-//     // считал строку и сразу же распарсил в json
-//     let jsonConfig: JSON = JSON.parse(fs.readFileSync(dest, "utf8"));
-//     let Pr: Presentation = jsonConfig;
-//     return Pr;
-// }
+/*          ошибка с полями
+function open(dest: string): Presentation {
+    // считал строку и сразу же распарсил в json
+    let jsonConfig: JSON = JSON.parse(fs.readFileSync(dest, "utf8"));
+    return jsonConfig;
+}*/
 
 function watch(Pr: Presentation) {
     // тут будет адская вёрстка
 }
 
-/*          сюда никому не заходить
-function undo(h: History): Presentation {
-    return h[-1]                            // не факт, что компилятор не заорёт
+function addStateToHistory(h:History, pr: Presentation): History {
+    // добавляет состояние презентации и контролит количество, чтобы не подпалить оперативу
+    if(h.length > MAX_HISTORY_SIZE) {
+        // убирает элемент из начала и закинывает в конец
+        h.shift(); h.push(pr);
+        return h;
+    } else {
+        h.push(pr);
+        return h;
+    }
 }
 
-function redo(pr: Presentation, h: History): Presentation {
-    if (pr != h[-1]) {
-        return h[-1]
-    }
-}*/
+
+/*      отменяет и возвращает, провека типов будет на уровне выше    */
+function undo(h: History): Presentation | undefined {
+    return h[h.length - 2];
+}
+function redo(h: History): Presentation | undefined {
+    return h[-1];
+}
