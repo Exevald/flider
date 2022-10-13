81abbb4a298394f29f6aa0d7fe1308103d4a6daa
Sergey <100339481+alozhka@users.noreply.github.com>
2022-10-13 23:58:05 +0300

    наработки по верхней панели

--- src/view/components/TopPanel/TopPanel.tsx
+++ src/view/components/TopPanel/TopPanel.tsx
@@ -1,10 +1,65 @@
-import style from "./TopPanel.module.css"
+import "./TopPanel.module.css"
+import logoArea from "./LogoArea.svg"
+const PRESENTATION_NAME_MAX_LENGTH = 48;
 
-const TopPanel = ({}) => {
+const LogoArea = () => {
+    return(
+        <div>
+            <img
+                src={logoArea}
+                alt={"Main Logo"}/>
+        </div>
+    )
+}
+
+const InputPresentationName = () => {
+    return(
+        <div>
+            <p>
+                <input name={"PrName"}
+                      className={"inputPresentationName"}
+                      maxLength={PRESENTATION_NAME_MAX_LENGTH}
+                      placeholder={"Презентация без названия"}
+                      accept={".json"}
+                />
+            </p>
+        </div>
+    )
+}
+
+const OpenBtn = () => {
+    return(
+        <div className={"openBtn"}>
+            <p>Открыть</p>
+        </div>
+    )
+}
+
+const SaveBtn = () => {
+    return(
+        <div className={"saveBtn"}>
+            <p>Сохранить</p>
+
+        </div>
+    )
+}
+
+const WatchBtn = () => {
+    return(
+        <div className={"watchBtn"}>
+            <p>Смотреть</p>
+        </div>
+    )
+}
+
+const TopPanel = ({/* надо параметры */}) => {
     return (
-        <div
-            className={``}
-        >
+        <div className={`top-panel`}>
+            <LogoArea/>
+            <InputPresentationName/>
+            <OpenBtn/>
+            <SaveBtn/>
+            <WatchBtn/>
         </div>
     )
 }
