73e214f597741a84068ca60f2c646fbefda8dd27
Konstantin Kalinin <kalinin.konst.official@gmail.com>
2022-10-25 14:33:40 +0300

    flider#7 Вставил иконки кнопок в toolBar.

--- src/view/Toolbar/Toolbar.tsx
+++ src/view/Toolbar/Toolbar.tsx
@@ -8,6 +8,14 @@ const Toolbar = () => {
     return (
         <div className={styles.toolbar}>
             <Button viewStyle={"add"} iconStyle={"center"} iconSrc={AddIcon} onClick={() => {}}></Button>
+            <ButtonIcon viewStyle={"undo"} iconStyle={"center"} onClick={() => {}}></ButtonIcon>
+            <ButtonIcon viewStyle={"redo"} iconStyle={"center"} onClick={() => {}}></ButtonIcon>
+            <ButtonIcon viewStyle={"selectArea"} iconStyle={"center"} onClick={() => {}}></ButtonIcon>
+            <ButtonIcon viewStyle={"selectArrow"} iconStyle={"center"} onClick={() => {}}></ButtonIcon>
+            <ButtonIcon viewStyle={"textArea"} iconStyle={"center"} onClick={() => {}}></ButtonIcon>
+            <ButtonIcon viewStyle={"image"} iconStyle={"center"} onClick={() => {}}></ButtonIcon>
+            <ButtonIcon viewStyle={"figure"} iconStyle={"center"} onClick={() => {}}></ButtonIcon>
+            <ButtonIcon viewStyle={"line"} iconStyle={"center"} onClick={() => {}}></ButtonIcon>
         </div>
     )
 }
