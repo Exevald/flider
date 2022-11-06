15ad9bd7e38d0aa7c2623d2682ed3daa05e9f2c5
Sergey <100339481+alozhka@users.noreply.github.com>
2022-11-06 23:07:12 +0300

    Добавил функцию создания презентаций

--- src/core/functions/PresentationFunctions.ts
+++ src/core/functions/PresentationFunctions.ts
@@ -4,48 +4,56 @@ import {History, Presentation, Slide} from "../../model/Types";
 
 
 /**         очень важные константы          **/
-const MAX_HISTORY_SIZE = 30;
-const DEFAULT_SLIDE_CONFIG: Slide = {
+export const MAX_HISTORY_SIZE = 30;
+export const DEFAULT_SLIDE_CONFIG: Slide = {
     id: "0",
     items: [],
     bgColor: "white",
 }
 
-function changeTitle(Pr: Presentation, title: string): Presentation {
+
+export function createPresentation(): Presentation {
+    let pr: Presentation;
+    pr = DEFAULT_SLIDE_CONFIG;
+    return pr;
+}
+
+
+export function changeTitle(Pr: Presentation, title: string): Presentation {
     Pr.title = title;
     return Pr
 }
 
 
     /**         СОХРАНЕНИЯ        **/
-function saveAsJSON(Pr: Presentation): string {
+export function saveAsJSON(Pr: Presentation): string {
     return JSON.stringify(Pr);
 }
 
-function saveAsPDF(Pr: Presentation) {
+export function saveAsPDF(Pr: Presentation) {
     // без понятия
 }
 
 /*          ошибка с полями
-function open(dest: string): Presentation {
+export function open(dest: string): Presentation {
     // считал строку и сразу же распарсил в json
     let jsonConfig: JSON = JSON.parse(fs.readFileSync(dest, "utf8"));
     return jsonConfig;
 }*/
 
-function watch(Pr: Presentation) {
+export function watch(Pr: Presentation) {
     // тут будет адская вёрстка
 }
 
     /**         ДЕЙСТВИЯ СО СЛАЙДАМИ         **/
-function createSlide(pr: Presentation): Presentation {
+export function createSlide(pr: Presentation): Presentation {
     pr.slides.push(DEFAULT_SLIDE_CONFIG);
     return pr;
 }
 
 
     /**         ДЕЙСТВИЯ С ИСТОРИЕЙ         **/
-function addStateToHistory(h:History, pr: Presentation): History {
+export function addStateToHistory(h:History, pr: Presentation): History {
     // добавляет состояние презентации и контролит количество, чтобы не подпалить оперативу
     if(h.length > MAX_HISTORY_SIZE) {
         // убирает элемент из начала и закинывает в конец
@@ -58,9 +66,9 @@ function addStateToHistory(h:History, pr: Presentation): History {
 }
 /*      отменяет и возвращает, провека типов будет на уровне выше
 *       ещё надо переставлять собитыя истории при добавлении события в центр */
-function undo(h: History): Presentation | undefined {
+export function undo(h: History): Presentation | undefined {
     return h[h.length - 2];
 }
-function redo(h: History): Presentation | undefined {
+export function redo(h: History): Presentation | undefined {
     return h[-1];
 }
