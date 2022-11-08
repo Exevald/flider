71aa57a65342a4ba73fdbf69034d0aa1f7583c93
Sergey <100339481+alozhka@users.noreply.github.com>
2022-11-08 14:41:53 +0300

    Поддержка действий пары кнопол в тулбре

--- src/App.tsx
+++ src/App.tsx
@@ -5,12 +5,14 @@ import './App.css';
 import TopPanel from "./view/TopPanel/TopPanel";
 import {Toolbar} from "./view/Toolbar/Toolbar";
 import {WorkSpace} from "./view/WorkSpace/WorkSpace";
+import {createPresentation} from "./core/functions/PresentationFunctions";
 
 function App() {
+    let pres = createPresentation()
     return (
         <div className="App">
             <TopPanel></TopPanel>
-            <Toolbar></Toolbar>
+            <Toolbar pr={pres}></Toolbar>
             <WorkSpace></WorkSpace>
         </div>
     );
