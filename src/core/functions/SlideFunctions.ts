cac9a0016983695e4c9ee556f979ddfaa75d7503
Sergey <100339481+alozhka@users.noreply.github.com>
2022-11-08 14:06:53 +0300

    Внёс константу слайда

--- src/core/functions/SlideFunctions.ts
+++ src/core/functions/SlideFunctions.ts
@@ -1,5 +1,12 @@
 import {Presentation, Slide} from "../../model/Types";
 
+/*          Константы           */
+const DEFAULT_SLIDE_CONFIG: Slide = {
+    id: "0",
+    items: [],
+    bgColor: "white",
+}
+
 function createSlide(
     presentation: Presentation,
 ): Presentation {
@@ -10,10 +17,13 @@ function createSlide(
         bgColor: "white"
     }
 
+    console.log("create slide");
+
     return {
         ...presentation
     }
 
 }
 
+export {DEFAULT_SLIDE_CONFIG}
 export {createSlide};
