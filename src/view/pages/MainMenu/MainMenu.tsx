beaef7d76eb84ac71ca19e7cf81a9fc4f4694316
Konstantin Kalinin <kalinin.konst.official@gmail.com>
2022-12-06 13:57:50 +0300

    flider#20 Доверстал экран меню

--- src/view/pages/MainMenu/MainMenu.tsx
+++ src/view/pages/MainMenu/MainMenu.tsx
@@ -5,24 +5,20 @@ import {Button} from "../../components/Button/Button";
 
 const MainMenu = () => {
     return (
-        <div>
-            <div className={styles.mainMenuPageWrapper}>
-                <div className={styles.mainMenuPageWrapper}>
-                    <div className={styles.titleWrapper}>
-                        <img src={titleImage} alt={"titleImage"}/>
-                    </div>
-                    <div className={styles.createPresentationButtonWrapper}>
-                        <Button viewStyle={"default"} onClick={() => {
-                        }} text={"Создать презентацию"}></Button>
-                    </div>
-                    <div className={styles.openPresentationButtonWrapper}>
-                        <Button viewStyle={"default"} onClick={() => {
-                        }} text={"Открыть презентацию"}></Button>
-                    </div>
-                    <div className={styles.logoImageWrapper}>
-                        <img src={logoImage} alt={"logoImage"}></img>
-                    </div>
-                </div>
+        <div className={styles.menuContainer}>
+            <div className={styles.titleWrapper}>
+                <img src={titleImage} alt={"titleImage"}/>
+            </div>
+            <div className={styles.createPresentationButtonWrapper}>
+                <Button viewStyle={"default"} onClick={() => {
+                }} text={"Создать презентацию"}></Button>
+            </div>
+            <div className={styles.openPresentationButtonWrapper}>
+                <Button viewStyle={"default"} onClick={() => {
+                }} text={"Открыть презентацию"}></Button>
+            </div>
+            <div className={styles.logoImageWrapper}>
+                <img src={logoImage} alt={"logoImage"}></img>
             </div>
         </div>
     )
