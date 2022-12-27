ed45f5c5a2d7fafc5b5b84b0b86215d30a4c3bce
Sergey <100339481+alozhka@users.noreply.github.com>
2022-12-27 15:10:17 +0300

    Вынес обработчик в useEffect

--- src/view/pages/PresentationPage/PresentationPage.tsx
+++ src/view/pages/PresentationPage/PresentationPage.tsx
@@ -2,22 +2,23 @@ import TopPanel from "../../components/TopPanel/TopPanel";
 import Toolbar from "../../components/Toolbar/Toolbar";
 import WorkSpace from "../../components/WorkSpace/WorkSpace";
 import {hideDropDownKeyboardPressed, handleClicks} from "../../components/DropDown/DropDown";
+import {useEffect} from "react";
 
 const PresentationPage = () => {
+
+    useEffect(() => {
+        const doc = document.querySelector('body');
+        if (doc !== null) {
+            doc.addEventListener("click", handleClicks)
+            return () => {
+                doc.removeEventListener("click", handleClicks)
+            }
+        }
+    });
+
     return (
         <div tabIndex={0} style={{outline: 0}}
              onKeyDown={hideDropDownKeyboardPressed}
-             onClick={() => {
-
-                 const doc = document.querySelector('body')
-                 if (doc !== null) {
-                     doc.addEventListener('click', handleClicks);
-                     setTimeout(() => {
-                         doc.removeEventListener('click', handleClicks)
-                     }, 1);
-                 }
-
-             }}
         >
             <TopPanel/>
             <Toolbar/>
