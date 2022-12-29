05cd832213f301254e9c72b8cf7c31f6fbdcd650
Sergey <100339481+alozhka@users.noreply.github.com>
2022-12-29 22:56:44 +0300

    Вынес время появления дропдауна в константу

--- src/core/functions/utility.ts
+++ src/core/functions/utility.ts
@@ -5,6 +5,9 @@ const COLOR_PICKER_COLORS = [
     "yellow", "orange", "darkorange", "red", "darkred"
 ];
 
+// надо менять ещё и в css стиле
+const DROPDOWN_ANIMATION_TIME = 220; //ms
+
 const MAX_HISTORY_SIZE = 30;
 const MAX_TITLE_SIZE = 12;
 
@@ -27,5 +30,5 @@ function max(a: number, b: number): number {
     return a > b ? a : b;
 }
 
-export {MAX_HISTORY_SIZE, COLOR_PICKER_COLORS, MAX_TITLE_SIZE};
+export {MAX_HISTORY_SIZE, COLOR_PICKER_COLORS, MAX_TITLE_SIZE, DROPDOWN_ANIMATION_TIME};
 export {getRandomId, min, max};
