2a9ff78cf8b1d708fd4bc5fdb180b8da6066daaf
Sergey <100339481+alozhka@users.noreply.github.com>
2023-01-11 23:14:54 +0300

    Добавил  внутреннюю стилизацию TextArea

--- src/view/components/TextArea/TextArea.tsx
+++ src/view/components/TextArea/TextArea.tsx
@@ -3,28 +3,32 @@ import topPanelStyles from "../TopPanel/TopPanel.module.css"
 import itemStyles from "../SlideItem/SlideItem.module.css"
 import {connect} from "react-redux";
 import {MAX_TITLE_SIZE} from "../../../core/functions/utility";
+import {CSSProperties} from "react";
 
 interface TextAreaProps {
     placeholder?: string;
     type: 'title' | 'ordinary' | 'slideItem';
     value?: string;
+    style?: CSSProperties,
     onKeyUp: (value: string) => void;
 }
 
 const TextArea = ({
                       placeholder = "",
                       value,
+                      style,
                       onKeyUp,
                       type
                   }: TextAreaProps) => {
     let textAreaStyle: string;
     type === 'title' ? textAreaStyle = topPanelStyles.name : textAreaStyle = `${styles.button_default} ${styles.inputDefault}`;
     if (type === 'slideItem') {
-        textAreaStyle = `${itemStyles.element} ${itemStyles.textArea}`
+        textAreaStyle = itemStyles.textArea
     }
     return (
         <input type="text"
                value={value}
+               style={style}
                placeholder={placeholder}
                className={textAreaStyle}
                onKeyUp={(event) => {
@@ -32,8 +36,7 @@ const TextArea = ({
                        onKeyUp(event.currentTarget.value)
                    }
                }}
-        >
-        </input>
+        />
     )
 }
 
